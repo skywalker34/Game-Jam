1056f5a0bda5c289acdd7cdf3f25df92524294da
skywalker34 <vincenthsieh34@gmail.com>
2023-09-19 00:12:53 -0400

    Game Jam 2023

---
+++ Game Jam 2023/Assets/Constant/Constants.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public static class Constants
+{
+    public const float horixontalSpeed = 10.0f;
+    public const float dashingSpeed = 30.0f;
+    public const float verticalSpeed = 40.0f;
+    public const float gravity = -100.0f;
+
+}
+
+public enum PlayerNumber
+{
+    one = 1, 
+    two = 2,
+}
+
+public enum  Movement
+{
+    up, 
+    down, 
+    left, 
+    right,
+    dash,
+    stop,
+}
---
+++ Game Jam 2023/Assets/Shooting.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public Transform shootPoint;
+    public float shootInterval = 0.01f;
+
+    private float lastShootTime;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Shoot();
+            lastShootTime = Time.time;
+        }
+    }
+
+    void Shoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, shootPoint.position, Quaternion.identity);
+
+        Rigidbody2D rigidbody = bullet.GetComponent<Rigidbody2D>();
+        rigidbody.velocity = new Vector2(10,0);
+    }
+}
